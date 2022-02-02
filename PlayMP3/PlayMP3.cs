57af8b8a740c530022335c16269a7bd181b3e6d0
Ehsan <JMSEhsan@gmail.com>
2022-02-01 23:50:01 -0700

    Add project files.

---
+++ PlayMP3/PlayMP3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlayMP3
+{
+
+    public partial class PlayMP3 : Form
+    {
+        public PlayMP3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
