c27117546e1f00baf470b40c1aadd68084e6d2e0
Ehsan <78066698+JMSEhsan@users.noreply.github.com>
2022-02-10 22:02:49 -0700

    Update PlayMP3.cs
    commit

--- PlayMP3/PlayMP3.cs
+++ PlayMP3/PlayMP3.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,20 +7,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
 
 namespace PlayMP3
 {
 
     public partial class PlayMP3 : Form
     {
+         WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer();
+        //private SoundPlayer Player = new SoundPlayer();
+     
         public PlayMP3()
-        {
+        {   
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Play_Click(object sender, EventArgs e)
         {
+            //try
+            //{
+            //    // Note: You may need to change the location specified based on
+            //    // the sounds loaded on your computer.
+            //    this.Player.SoundLocation = @"C:\Users\se_ja\OneDrive\Documents\Projects\PlayMp3CS\PartyingInRussia.mp3";
+            //    this.Player.PlayLooping();
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message, "Error playing sound");
+            //}
+
+            player.URL = /* @"C:\Windows\Media\chimes.wav"; */ @"C:\Users\se_ja\OneDrive\Documents\Projects\PlayMp3CS\PartyingInRussia.mp3";
+                                                         // player.settings.setMode("loop", true);
+            
+            player.controls.play();
+         //   player.settings.setMode("loop", false);
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+           player.controls.stop();
+        }
+
+        private void checkBoxLoop_CheckedChanged(object sender, EventArgs e)
+        {
+            // player.URL = @"C:\Users\se_ja\OneDrive\Documents\Projects\PlayMp3CS\PartyingInRussia.mp3";
+            if (checkBox1.Checked)
+            {
+                player.settings.setMode("loop", true);
+            }
+            else
+            {
+                player.settings.setMode("loop", false);
+            }
+                
+           // player.controls.play();
 
         }
+     
     }
 }
